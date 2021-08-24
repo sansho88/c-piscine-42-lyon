9248cc1eb50f240d4955ce73342c076a9c08493f
TGriffit <tgriffit@student.42lyon.fr>
2021-08-24 15:47:42 +0200

    Rush01 + Rush02 + C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgriffit <tgriffit@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/24 12:18:45 by tgriffit          #+#    #+#             */
+/*   Updated: 2021/08/24 14:50:09 by tgriffit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define EVEN(nbr) (nbr % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+#define t_bool int
+enum e_bool {
+	FALSE,
+	TRUE
+};
+void	ft_putstr(char *str);
+t_bool	ft_is_even(int nbr);
+#endif
