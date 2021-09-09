f2772bd9072ac7f4820e8f7c5fb61dc295077abe
Sansho <sansho88@hotmail.fr>
2021-09-09 11:42:06 +0200

    Update du read-file

--- traininig/concepts/readfile.c
+++ traininig/concepts/readfile.c
@@ -6,60 +6,61 @@
 /*   By: tgriffit <tgriffit@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 12:01:39 by tgriffit          #+#    #+#             */
-/*   Updated: 2021/09/08 15:53:39 by tgriffit         ###   ########lyon.fr   */
+/*   Updated: 2021/09/09 11:09:04 by tgriffit         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unistd.h"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
-/*
-void ft_readfile(char *file)
+int ft_readfile()
 {
     int fd;
     int size;
-    char    txt;
+    char    txt[10000]; //C'est tres degueu', il vaut mieux faire 2 boucles: 1 pour lire la taille du texte,
+    // puis malloc et son check, puis 2eme boucle pour ecrire dans le char *.
 
-    fd = open(file, O_RDWR);
+    /*Seulement le full path fonctionne ici, sinon errno = "no file or directory"... particularité windows? */
+    fd = open("C:\\Users\\sansh\\CLionProjects\\piscine42\\traininig\\concepts\\mytext.txt", O_RDWR);
+    if (fd == -1)
+        return errno;
     size = _read(fd, &txt, sizeof(txt));
-    //_write(1, txt, size);
+    _write(1, txt, size);
+    return 0;
 }
- */
+
 
 
 //version OC
 // https://openclassrooms.com/forum/sujet/comment-ca-fonctionne-12569
+/*
 int ft_readfile() {
     int fd;
-/* Un buffer */
+// Un buffer
     //char buf[1128];
     char *cpy;
     char c;
-/* Taille lue : */
+// Taille lue :
     int size;
     int i;
 
-/* On ouvre le fichier : en fait on recupere un fd sur le fichier*/
+// On ouvre le fichier : en fait on recupere un fd sur le fichier
     fd = _open("C:\\Users\\sansh\\CLionProjects\\piscine42\\traininig\\concepts\\mytext.txt", O_RDONLY, S_IREAD);
-/* il ne peut pas y avoir de fd negatif,
-** si open nous retourne -1 c'est que l'ouverture a echoue
-** dans ce cas on quitte
-*/
+// il ne peut pas y avoir de fd negatif,
+// si open nous retourne -1 c'est que l'ouverture a echoue
+// dans ce cas on quitte
+
     if (fd == -1)
         return errno;
+//
+// On lit :
+// on passe a read :
+// - le fd,
+// - le buffer
+// - la taille du buffer
+// Attention si tu passe une taille de buffer plus grande que la taille de ton buffer,
+// ton programme deviens sensible aux Buffer Overflow
 
-
-/*
-** On lit :
-** on passe a read :
-** - le fd,
-** - le buffer
-** - la taille du buffer
-** Attention si tu passe une taille de buffer plus grande que la taille de ton buffer,
-** ton programme deviens sensible aux Buffer Overflow
-*/
     size = 0;
     while (read(fd, &c, sizeof(c)))
         size++;
@@ -67,7 +68,7 @@ int ft_readfile() {
     if (!cpy)
         return (4);
     i = 0;
-    /*Après avoir calculé la taille, on reset la tête de lecture au début */
+    //Après avoir calculé la taille, on reset la tête de lecture au début
     close(fd);
     fd = _open("C:\\Users\\sansh\\CLionProjects\\piscine42\\traininig\\concepts\\mytext.txt", O_RDONLY, S_IREAD);
     while (read(fd, &c, sizeof(c)))
@@ -79,26 +80,27 @@ int ft_readfile() {
 
     return size;
 
-/*
-** On raoute un '\0' à la fin de la chaine lut, pour être sur d'avoir une chaine de caractères valide.
-** size correspondant a l'index du dernier caractere du buffer + 1.
-** Ceci est utile si tu veux utiliser ta chaine dans une fonction comme strcmp() ou printf()
-*/
+//
+// On raoute un '\0' à la fin de la chaine lut, pour être sur d'avoir une chaine de caractères valide.
+// size correspondant a l'index du dernier caractere du buffer + 1.
+// Ceci est utile si tu veux utiliser ta chaine dans une fonction comme strcmp() ou printf()
+
 
     //buf[size] = '\0';
 
-/*
-** On affiche ce que l'on viens de lire dans la console :
-** NOTE :
-** il existe des FD speciaux :
-** Le fd 1 est la sortie standart ( console )
-*/
+//
+// On affiche ce que l'on viens de lire dans la console :
+// NOTE :
+// il existe des FD speciaux :
+// Le fd 1 est la sortie standart ( console )
+
 
     //_write(1, buf, size);
 
-/* Ne pas oublier de libérer ton file descriptor */
+// Ne pas oublier de libérer ton file descriptor
     close(fd);
 
     return 0;
 
 }
+*/
